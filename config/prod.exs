d547b5dc894074c38a968f4038176bdc8eec2607
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2016-06-14 21:05:37 -0700

    better hostname

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :movies_api, MoviesApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "blooming-spire-32320.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "macedo-movies-api.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
